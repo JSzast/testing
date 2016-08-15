b2cb1f4210af3596871f3ced74a9135262f3fe43
Maksymilian Sielicki <maksymiliansielicki@gmail.com>
2016-08-15 02:06:25 +0200

    Gruntfile configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,40 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
-          paths: ['css']
+          paths: ['css'],
+          compress: false,
         },
         files: {
-        'css/result.css': 'less/style.less'
+          'css/result.css': 'less/style.less'
         }
+      },
+      production: {
+        options: {
+          paths: ['css'],
+          compress: true,
+          sourceMap: true,
+          sourceMapFilename: 'sorsmapa'
+        },
+        files: {
+          'css/result.css': 'less/style.less'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.less',
+        tasks: ['default']
+        // options: {
+        //   livereload: true
+        // }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['less']);
+  grunt.registerTask('default', ['less', 'watch']);
 
   // grunt.registerTask('logger', '', function() {
   //   console.log('This grunt task is pointless.');
